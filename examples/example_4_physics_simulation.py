255476425959ed8c22a007afcb6e8278dcdfa935
Romain Desarzens <hib.pub@protonmail.com>
2025-10-08 17:29:04 +0200

    Naive integration of controller and inverse kinematics

--- examples/example_4_physics_simulation.py
+++ examples/example_4_physics_simulation.py
@@ -1,40 +1,18 @@
+import numpy as np
 import pybullet as pb
 import pybullet_data
 import os
+import pinocchio as pin
+
+from lipm_walking_controller.inverse_kinematic import InvKinSolverParams, solve_inverse_kinematics
+
+from lipm_walking_controller.controller import (
+    PreviewControllerParams,
+    compute_preview_control_matrices,
+    update_control,
+)
 
 from lipm_walking_controller.model import Talos
-
-
-def track_q_position(robot_id, pin_model, q, name_to_bid, kp=80.0, kd=2.0, fmax=200.0):
-    # 1) Base: choose ONE option
-    # A) During early tests, fix base:
-    #   pb.resetBasePositionAndOrientation(robot_id, q[0:3], q[3:7])
-    #   # Load robot with useFixedBase=True to prevent collapse.
-    # B) Or constrain a stance foot to the world (see lock_foot below).
-
-    # 2) Joints: drive each to q_des with PD
-    idx = 7  # after free-flyer
-    bids, qdes = [], []
-    for j in pin_model.joints[1:]:  # skip universe
-        if j.nq == 1:
-            jname = pin_model.names[j.id]
-            if jname in name_to_bid:
-                bids.append(name_to_bid[jname])
-                qdes.append(float(q[idx]))
-            idx += j.nq
-        else:
-            idx += j.nq
-
-    if bids:
-        pb.setJointMotorControlArray(
-            bodyUniqueId=robot_id,
-            jointIndices=bids,
-            controlMode=pb.POSITION_CONTROL,
-            targetPositions=qdes,
-            positionGains=[kp] * len(bids),
-            velocityGains=[kd] * len(bids),
-            forces=[fmax] * len(bids),
-        )
 
 
 def apply_position(q_des, j_to_q_idx):
@@ -57,6 +35,26 @@ if __name__ == "__main__":
     pb.setGravity(0, 0, -9.81)
     pb.setPhysicsEngineParameter(fixedTimeStep=dt, numSolverIterations=50, erp=0.2, contactERP=0.2)
 
+    # Preview controller parameters
+    t_preview = 1.6  # Time horizon used for the preview controller
+
+    # ZMP reference parameters
+    t_ss = 0.6  # Single support phase time window
+    t_ds = 0.1  # Double support phase time window
+    n_steps = 25
+    l_stride = 0.3
+    max_height_foot = 0.05
+
+    ctrler_params = PreviewControllerParams(
+        zc=0.89,
+        g=9.81,
+        Qe=1.0,
+        Qx=np.zeros((3, 3)),
+        R=1e-6,
+        n_preview_steps=int(round(t_preview / dt)),
+    )
+    ctrler_mat = compute_preview_control_matrices(ctrler_params, dt)
+
     plane = pb.loadURDF("plane.urdf")
     PKG_PARENT = os.path.expanduser(os.environ.get("PKG_PARENT", "~/projects"))
     URDF = os.path.join(PKG_PARENT, "talos_data/urdf/talos_full.urdf")
@@ -74,7 +72,42 @@ if __name__ == "__main__":
     q = talos.set_and_get_default_pose()
 
     k = 0
+    x_k = np.array([0.0, 0.0, 0.0, 0.0], dtype=float)
+    y_k = np.array([0.0, 0.0, 0.0, 0.0], dtype=float)
+
+    ik_sol_params = InvKinSolverParams(
+        fixed_foot_frame=talos.right_foot_id,
+        moving_foot_frame=talos.left_foot_id,
+        torso_frame=talos.torso_id,
+        model=talos.model,
+        data=talos.data,
+        w_torso=10.0,
+        w_com=10.0,
+        w_mf=10.0,
+        w_ff=1000.0,
+        mu=1e-5,
+        dt=dt,
+    )
+
+    oMf_rf0 = talos.data.oMf[talos.right_foot_id].copy()
+
     while True:
+        # Get zmp ref horizon
+        zmp_ref_horizon = np.zeros((ctrler_params.n_preview_steps - 1, 2))
+
+        _, x_k, y_k = update_control(
+            ctrler_mat, np.array([0.0, 0.0]), zmp_ref_horizon, x_k.copy(), y_k.copy()
+        )
+
+        # The CoM target is meant to follow the computed x and y and stay at constant height zc from the feet
+        com_target = np.array([x_k[1], y_k[1], ctrler_params.zc])
+
+        # Stabilize at the position
+        ik_sol_params.fixed_foot_frame = talos.left_foot_id
+        ik_sol_params.moving_foot_frame = talos.right_foot_id
+
+        q_new, dq = solve_inverse_kinematics(q, com_target, oMf_rf0, ik_sol_params)
+
         apply_position(q_des=q, j_to_q_idx=map_joint_idx_to_q_idx)
         pb.stepSimulation()
         k += 1
