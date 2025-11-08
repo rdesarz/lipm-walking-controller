631fff0ced28297b7db946dbde143feaa8304d9e
Romain Desarzens <hib.pub@protonmail.com>
2025-11-08 17:03:10 +0100

    Refactor foot step generation

--- examples/example_4_physics_simulation.py
+++ examples/example_4_physics_simulation.py
@@ -6,7 +6,11 @@ import numpy as np
 import pybullet as pb
 import pinocchio as pin
 
-from biped_walking_controller.foot import compute_feet_path_and_poses, BezierCurveFootPathGenerator
+from biped_walking_controller.foot import (
+    compute_feet_path_and_poses,
+    BezierCurveFootPathGenerator,
+    SinusoidFootPathGenerator,
+)
 
 from biped_walking_controller.inverse_kinematic import InvKinSolverParams, solve_inverse_kinematics
 from biped_walking_controller.plot import plot_feet_and_com
@@ -45,9 +49,9 @@ def main():
     t_ds = 0.4  # Double support phase time window
     t_init = 4.0  # Initialization phase (transition from still position to first step)
     t_end = 1.0
-    n_steps = 25  # Number of steps executed by the robot
+    n_steps = 15  # Number of steps executed by the robot
     l_stride = 0.1  # Length of the stride
-    max_height_foot = 0.015  # Maximal height of the swing foot
+    max_height_foot = 0.01  # Maximal height of the swing foot
 
     # Preview controller parameters
     t_preview = 1.6  # Time horizon used for the preview controller
