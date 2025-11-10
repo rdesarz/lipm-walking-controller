9da43dc0fc498fc49f7a54474054a4f3d4f4041e
Romain Desarzens <hib.pub@protonmail.com>
2025-11-10 14:42:31 +0100

    Add unit tests

--- tests/test_foot.py
+++ tests/test_foot.py
@@ -10,10 +10,49 @@ class TestWalkingPhase(unittest.TestCase):
     def test_begin_double_support(self):
         phase = WalkingPhase.DOUBLE_SUPPORT
 
-        # When we reach the time threshold, we switch to a
         result = compute_walking_phase(phase, 0.0, self.params)
 
         self.assertIsNone(result)
+
+    def test_switch_to_single_support(self):
+        phase = WalkingPhase.DOUBLE_SUPPORT
+
+        result = compute_walking_phase(phase, self.params.t_ds + 0.1, self.params)
+
+        self.assertEqual(result, phase.SINGLE_SUPPORT)
+
+    def test_do_not_switch_to_ds_if_beginning_of_phase(self):
+        phase = WalkingPhase.SINGLE_SUPPORT
+
+        result = compute_walking_phase(
+            phase, 0.0, self.params, contact_force=self.params.force_threshold + 10
+        )
+
+        self.assertIsNone(result)
+
+    def test_do_not_switch_to_ds_if_force_too_low(self):
+        phase = WalkingPhase.SINGLE_SUPPORT
+
+        result = compute_walking_phase(
+            phase,
+            self.params.t_ss * 0.75,
+            self.params,
+            contact_force=self.params.force_threshold - 10,
+        )
+
+        self.assertIsNone(result)
+
+    def test_switch_to_ds_if_force_too_low_and_phase_close_to_end(self):
+        phase = WalkingPhase.SINGLE_SUPPORT
+
+        result = compute_walking_phase(
+            phase,
+            self.params.t_ss * 0.75,
+            self.params,
+            contact_force=self.params.force_threshold + 10,
+        )
+
+        self.assertEqual(result, phase.DOUBLE_SUPPORT)
 
 
 if __name__ == "__main__":
